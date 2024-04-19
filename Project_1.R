60b49b3acb8d560ea15554b4b2535548a3245da6
lana1218 <lanahuynh12@yahoo.com>
2024-04-19 11:53:55 -0700

    created map and added filters

--- Project_1.R
+++ Project_1.R
@@ -1,62 +1,138 @@
-library(tidyverse)
-library(here)
-library(shiny)
+
+# Source for leaflet in a shiny app: https://rstudio.github.io/leaflet/articles/shiny.html
 
 
-# Load required libraries
+
+# library(shiny)
+# library(leaflet)
+# library(tidyverse)
+# library(here)
+# 
+# df_911 <- read.csv(here("Data", "Seattle_small_911.csv"))
+# 
+# clean_911 <- df_911 |>
+#   mutate(Datetime = mdy_hms(Datetime),
+#          Year = year(Datetime)) |>
+#   filter(Year == 2023,
+#          Type %in% c("Aid Response",
+#                      "Medic Response",
+#                      "Auto Fire Alarm",
+#                      "Trans to AMR",
+#                      "Aid Response Yellow",
+#                      "Motor Vehicle Accident",
+#                      "Automatic Fire Alarm Resd",
+#                      "MVI - Motor Vehicle Incident"))
+# 
+# 
+# ui <- fluidPage(
+#   leafletOutput("mymap")
+# )
+# 
+# server <- function(input, output) {
+# 
+#   output$mymap <- renderLeaflet({
+#     leaflet() |>
+#       addProviderTiles(providers$Esri.NatGeoWorldMap) |>
+#       addCircleMarkers(data = clean_911,
+#                        lat = ~Latitude,
+#                        lng = ~Longitude)
+#   })
+# }
+# 
+# shinyApp(ui, server)
+
+
 library(shiny)
 library(leaflet)
+library(tidyverse)
+library(here)
 
-# Define example data
 df_911 <- read.csv(here("Data", "Seattle_small_911.csv"))
 
 clean_911 <- df_911 |>
   mutate(Datetime = mdy_hms(Datetime),
-         Year = year(Datetime))
+         Year = year(Datetime),
+         Month = months(Datetime)) |>
+  filter(Year == 2023,
+         Type %in% c("Aid Response",
+                     "Medic Response",
+                     "Auto Fire Alarm",
+                     "Trans to AMR",
+                     "Aid Response Yellow",
+                     "Motor Vehicle Accident",
+                     "Automatic Fire Alarm Resd",
+                     "MVI - Motor Vehicle Incident"))
 
-# Define UI for application
+# Unique types for type dropdown menu
+types <- unique(clean_911$Type)
+
+# Unique months for month dropdown menu
+months <- unique(clean_911$Month)
+
 ui <- fluidPage(
-
-  titlePanel("Map Filter"),
-
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("type", "Select Type:", choices = c("All", unique(clean_911$Type))),
-      sliderInput("year", "Select Year Range:", min = min(clean_911$Year), max = max(clean_911$Year), value = c(min(clean_911$Year), max(clean_911$Year)))
+  tags$head(
+    tags$style(HTML("
+      .map-container {
+        position: relative;
+        height: 800px;
+      }
+      .filter-bar {
+        position: absolute;
+        top: 10px;
+        right: 20px;
+        z-index: 1000;
+        background-color: white;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+      }
+    "))
+  ),
+  fluidRow(
+    column(width = 8,
+           div(class = "map-container",
+               leafletOutput("mymap")
+           )
     ),
-    mainPanel(
-      leafletOutput("map")
+    column(width = 4,
+           div(class = "filter-bar",
+               selectInput("type_filter", "Select Type:",
+                           choices = c("All", types)),
+               selectInput("month_filter", "Select Month:",
+                           choices = c("All", as.character(months)))
+           )
     )
   )
 )
 
-# Define server logic
 server <- function(input, output) {
-
-  filteredData <- reactive({
-    req(input$Type)
-    req(input$Year)
-    if (input$Type == "All") {
-      filtered <- data[clean_911$Year >= input$Year[1] & clean_911$Year <= input$Year[2], ]
-    } else {
-      filtered <- clean_911[clean_911$Type == input$Type & clean_911$Year >= input$Year[1] & clean_911$Year <= input$Year[2], ]
+  
+  filtered_data <- reactive({
+    data <- clean_911
+    
+    if (input$type_filter != "All") {
+      data <- filter(data, Type == input$type_filter)
     }
-    filtered
+    
+    if (input$month_filter != "All") {
+      data <- filter(data, Month == input$month_filter)
+    }
+    
+    return(data)
   })
-
-  output$map <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles("Esri.WorldImagery") %>%
-      addTiles() %>%
-      addCircleMarkers(data = clean_911, lat = ~Latitude,
-                       lng = ~Longitude,
-                       opacity = ~5) %>%
-      addMarkers(data = filteredData(), popup = ~paste("Type:", Type, "<br>", "Year:", Year))
+  
+  output$mymap <- renderLeaflet({
+    leaflet() |>
+      addProviderTiles(providers$Esri.NatGeoWorldMap) |>
+      addCircleMarkers(data = filtered_data(),
+                       lat = ~Latitude, 
+                       lng = ~Longitude)
   })
 }
 
-# Run the application
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
 
 
-# Source for leaflet in a shiny app: https://rstudio.github.io/leaflet/articles/shiny.html
+
+
+
